502317c11df131342f6f6674d499cf1ca2a9cb91
Felipe Vitor Espada <felipevitorespada23@gmail.com>
2026-05-27 14:33:20 -0300

    chore: add partials header and footer to sobre page

---
+++ partials/footer-sobre.php
@@ -0,0 +1,111 @@
+<link rel="stylesheet" href="\Casas-Brasilite\partials-css\footer.css">
+<footer class="rodape-completo">
+    <div class="newsletter">
+        <div class="container newsletter-conteudo">
+            <div class="texto-news">
+                <i class="far fa-envelope"></i>
+                <span>Receba ofertas exclusivas e novidades por e-mail</span>
+            </div>
+
+            <form class="form-news">
+                <input type="email" placeholder="Seu melhor e-mail">
+                <button type="submit" class="btn btn-laranja">Cadastrar</button>
+            </form>
+        </div>
+    </div>
+
+    <div class="container conteudo-rodape">
+
+        <div class="coluna-rodape">
+            <div class="logo">
+                <img src="../../imagens/logo2.png">
+            </div>
+
+            <p class="texto-descricao-rodape">
+                Materiais de construção com qualidade, segurança e o melhor
+                custo-benefício para sua obra.
+            </p>
+
+            <div class="redes-sociais">
+
+                <a href="#" target="_blank" rel="noopener noreferrer" aria-label="YouTube">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+                        <path d="M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93 .502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z" />
+                    </svg>
+                </a>
+
+                <a href="#" target="_blank" rel="noopener noreferrer" aria-label="WhatsApp">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+                        <path d="M.057 24l1.687-6.163c-1.041-1.804-1.588-3.849-1.587-5.946C.16 5.348 5.497 0 12.048 0c3.179.001 6.164 1.24 8.409 3.488 2.245 2.248 3.481 5.236 3.48 8.414-.003 6.557-5.338 11.907-11.89 11.907-2.013-.001-3.993-.513-5.747-1.49L0 24zm6.611-3.619c1.613.957 3.197 1.462 4.819 1.463 5.419 0 9.827-4.416 9.83-9.85.001-2.632-1.022-5.105-2.883-6.97C16.53 3.159 14.062 2.13 11.432 2.13c-5.425 0-9.833 4.417-9.836 9.853-.001 1.764.464 3.49 1.345 5.02l-.997 3.642 3.724-.964zm11.196-4.996c-.3-.152-1.78-.883-2.056-.984-.275-.101-.476-.151-.676.152-.199.303-.775.984-.95 1.186-.175.203-.35.228-.65.076-.3-.152-1.267-.468-2.415-1.496-.893-.799-1.496-1.786-1.671-2.09-.175-.303-.019-.467.132-.618.136-.135.301-.354.452-.531.15-.177.2-.303.3-.506.1-.202.05-.379-.025-.531-.075-.152-.676-1.644-.926-2.25-.244-.587-.49-.508-.676-.517-.175-.008-.376-.01-.576-.01-.201 0-.527.076-.802.379-.275.303-1.053 1.037-1.053 2.53 0 1.493 1.077 2.933 1.227 3.136.15.202 2.122 3.24 5.141 4.545.718.311 1.278.497 1.714.637.722.23 1.378.197 1.897.12.577-.087 1.78-.73 2.031-1.436.25-.706.25-1.312.175-1.436-.075-.124-.275-.201-.576-.352z" />
+                    </svg>
+                </a>
+
+                <a href="#" target="_blank" rel="noopener noreferrer" aria-label="Facebook">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+                        <path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z" />
+                    </svg>
+                </a>
+
+                <a href="#" target="_blank" rel="noopener noreferrer" aria-label="Instagram">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+                        <path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zM12 0C8.741 0 8.333.014 7.053.072 2.695.272.273 2.69.073 7.051.014 8.333 0 8.741 0 12c0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98C15.668.014 15.259 0 12 0zm0 5.838a6.162 6.162 0 1 0 0 12.324 6.162 6.162 0 0 0 0-12.324zM12 16a4 4 0 1 1 0-8 4 4 0 0 1 0 8zm6.406-11.845a1.44 1.44 0 1 0 0 2.881 1.44 1.44 0 0 0 0-2.881z" />
+                    </svg>
+                </a>
+
+            </div>
+        </div>
+
+        <div class="coluna-rodape">
+            <h4>Institucional</h4>
+
+            <ul>
+                <li><a href="#">Sobre nós</a></li>
+                <li><a href="#">Trabalhe conosco</a></li>
+                <li><a href="#">Política de privacidade</a></li>
+                <li><a href="#">Termos de uso</a></li>
+            </ul>
+        </div>
+
+        <div class="coluna-rodape">
+            <h4>Atendimento</h4>
+
+            <ul>
+                <li><a href="#">Central de ajuda</a></li>
+                <li><a href="#">Fale conosco</a></li>
+                <li><a href="#">Trocas e devoluções</a></li>
+                <li><a href="#">WhatsApp</a></li>
+            </ul>
+        </div>
+
+        <div class="coluna-rodape">
+            <h4>Categorias</h4>
+
+            <ul>
+                <li><a href="#">Materiais</a></li>
+                <li><a href="#">Ferramentas</a></li>
+                <li><a href="#">Estruturas</a></li>
+                <li><a href="#">EPI</a></li>
+                <li><a href="#">Ofertas</a></li>
+            </ul>
+        </div>
+
+        <div class="coluna-rodape">
+            <h4>Formas de pagamento</h4>
+
+            <div class="icones-pagamento">
+                <span class="cartao-falso">VISA</span>
+                <span class="cartao-falso">MASTER</span>
+                <span class="cartao-falso">ELO</span>
+                <span class="cartao-falso">HIPER</span>
+                <span class="cartao-falso pix">PIX</span>
+            </div>
+        </div>
+
+    </div>
+
+    <div class="rodape-inferior">
+        <div class="container">
+            <p>&copy; 2026 Casas Brasilite. Todos os direitos reservados.</p>
+        </div>
+    </div>
+</footer>
---
+++ partials/header-sobre.php
@@ -0,0 +1,16 @@
+<header>
+    <div class="header-container container">
+        <div class="logo">
+            <a href="/Casas-Brasilite/index.php">
+                <img src="../../imagens/logo1.png" alt="Logo Casas Brasilite">
+            </a>
+        </div>
+
+        <ul class="menu">
+            <li><a href="\Casas-Brasilite\janelas\sobre\sobre.php">Sobre</a></li>
+            <li><a href="\Casas-Brasilite\janelas\sobre\termos.php">Termos</a></li>
+            <li><a href="\Casas-Brasilite\janelas\sobre\contato.php">Contato</a></li>
+            <li><a href="\Casas-Brasilite\janelas\sobre\ajuda.php">Ajuda</a></li>
+        </ul>
+    </div>
+</header>
